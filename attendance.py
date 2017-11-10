d8563d808e1fe4d9c8562bb51245c41ad415d02c
Kevin Yaiko <itd07@yahoo.com>
2017-11-10 15:57:10 +0100

    Reorganize code into main, course_list and absences functions. Fix syntax of for loop in absences.

--- attendance.py
+++ attendance.py
@@ -1,32 +1,42 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('attendance.xlsx')
-sheet = wb.get_sheet_by_name('attendance')
-print(sheet['A1'].value)
-print("Cell {} is {}".format(sheet['A1'].coordinate, sheet['A1'].value))
+def main():
+    wb = openpyxl.load_workbook('attendance.xlsx')
+    sheet = wb.get_sheet_by_name('attendance')
+    print(sheet['A1'].value)
+    print("Cell {} is {}".format(sheet['A1'].coordinate, sheet['A1'].value))
 
-a = sheet.max_row
-print(a)
+    a = sheet.max_row
+    print(a)
 
-items = set()
-for cellObj in sheet['C']:
-    if cellObj.value == 'Course Code' or cellObj.value == None:
-        continue
-    else:
-        items.add(cellObj.value)
-print(items)
-courses = {}
-for name in items:
-    courses[name] = 0
-print(courses)
+    absenses(sheet, course_list(sheet))
 
-#Beginning to tally absenses
-#Check for absent value or tardy value
-#Find what row it's in
-#Go to column C of that row and find that value
-#Update the dictionary value of that value
-for value in sheet['L']:
-    if value == 'absent':
 
-courses['Discrete Mathematics'] += 1
-print(courses)
+def course_list(sheet):
+    courses = {}
+    items = set()
+    for cellObj in sheet['C']:
+        if cellObj.value == 'Course Code' or cellObj.value == None:
+            continue
+        else:
+            items.add(cellObj.value)
+    for name in items:
+        courses[name] = 0
+    print(items)
+    print(courses)
+    return courses
+
+
+def absenses(sheet, course_dict):
+    #Beginning to tally absenses
+    #Check for absent value or tardy value
+    #Find what row it's in
+    #Go to column C of that row and find that value
+    #Update the dictionary value of that value
+    for value in sheet['L']:
+        if value.value == 'absent':
+            course_dict['Discrete Mathematics'] += 1
+    print(course_dict)
+    return course_dict
+
+main()
